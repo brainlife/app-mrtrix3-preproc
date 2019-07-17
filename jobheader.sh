2b333682ae1331409fd0c3207d4ee62bb01abc55
Soichi Hayashi <hayashis@iu.edu>
2019-07-17 01:05:51 -0400

    Update jobheader.sh
    increasing timeout

--- jobheader.sh
+++ jobheader.sh
@@ -2,7 +2,7 @@
 
 ##figure out amount of wall time we really need by parsing config.json with jq
 if [ "$(jq .rpe -r config.json)" == "none" ]; then
-    walltime=01:30:00
+    walltime=03:00:00
 else
     walltime=24:00:00
 fi
